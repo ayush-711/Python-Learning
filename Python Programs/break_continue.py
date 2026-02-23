db1d0f9b8221dfb8d29281be6705a71c3ee39345
Ayush Kumar <ayush.kumar@trangile.net>
2026-02-23 11:56:19 +0530

    break_continue_pass statements

--- Python Programs/break_continue.py
+++ Python Programs/break_continue.py
@@ -1,13 +1,30 @@
-av_candy = 4
+# av_candy = 4
 
-ask = int(input("How many candies do you want? : "))
-i = 1
+# ask = int(input("How many candies do you want? : "))
+# i = 1
 
-while i <= ask:
-    if i > av_candy :  
-        print("out of stock")
-        break
+# while i <= ask:
+#     if i > av_candy :  
+#         print("out of stock")
+#         break
 
-    print(i , "candy" )
-    i = i + 1
+#     print(i , "candy" )
+#     i = i + 1
+
+# i = 1
+# for i in range (101):
+#     if i%2 == 0 or i%3 == 0:
+#         continue
+#     print(i)
+
+i = 1 
+for i in range (101):
+    if(i%2 == 0):
+        pass 
+
+    else:
+        print(i)
+
+
+
 
