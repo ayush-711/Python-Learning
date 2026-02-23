48a1623b9b0038dca4235bd3f853dd85395447b9
Ayush Kumar <ayush.kumar@trangile.net>
2026-02-23 12:46:41 +0530

    pattern printing

---
+++ patterns.py
@@ -0,0 +1,38 @@
+# WAP to print this - ####
+                      ####
+                      ####
+                      ####
+
+
+# for i in range (4):
+#     for j in range (4):
+#         print("#"  , end = " ")
+#     print()
+
+# WAP to print this - #
+                      ##
+                      ###
+                      ####
+
+
+# for i in range(5):
+#     for j in range(5):
+#         print("#" * i)
+#         break
+#     # print()
+
+# WAP to print this - ####
+                      ###
+                      ###
+                      #
+
+for i in range(4):
+    for j in range(4-i):
+        print("#", end = "")
+    print()
+
+
+
+    
+
+    
